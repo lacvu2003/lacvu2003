2bd250212875e00b4e3837998c2730510ef32663
lacvu2003 <136335910+lacvu2003@users.noreply.github.com>
2023-07-02 14:54:34 +0700

    Delete quanlysanpham directory

--- quanlysanpham/ADMIN/config/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-$conn = new mysqli("localhost","root","","quanlysanpham");
-if ($conn -> connect_errno) {
-  echo "Failed to connect to MySQL: " . $conn -> connect_error;
-  exit();
-}
-mysqli_set_charset($conn,"utf8_general_ci");
-?>
